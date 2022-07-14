c52600d83b19138e6e5c208fd9ca2d4ed2638f23
Alex Ross <alxrg@me.com>
2022-07-13 22:35:31 -0500

    completed the three data visualizations

--- script_classSurvive.js
+++ script_classSurvive.js
@@ -4,75 +4,111 @@ async function handleData() {
 
   let data = await d3.json('titanic-passengers.json')
 
-  // const getFaresAndAges = (data) => {
-    
-    // const cabins = data.filter((cabin) => cabin.fields.cabin && cabin.fields.survived)
-    // console.log(cabins)
-    // const filterCabin = cabins.filter((cabin) => cabin !== undefined)
-    // console.log(filterCabin)
-    // const firstLetterCabins = filterCabin.map((cabin) => cabin[0])
-    // console.log(firstLetterCabins)
-    // const fareLength = filterFare.length
-    // console.log(fareLength)
-    // const age = data.filter((passenger) => passenger.fields.age & passenger.fields.fare);
-    // console.log(age)
-    // return age
-  // }
+  const cabins = data.filter((cabin) => cabin.fields.cabin && cabin.fields.survived)
+  console.log(cabins)
 
-  // console.log(getFaresAndAges(data));
-
-  // const getFirstLetterCabins = (data, property) => {
-  //   const filteredResults = data.filter((filter) => filter.fields[property] !== undefined)
-  //   const getAllUniqueValues = filteredResults.reduce((acc, passenger) => {
-  //     const propertyValues = passenger.fields[property][0]
-  //     return { [propertyValues]: propertyValues, ...acc }
-  //   }, {}) 
+  const getFirstLetterCabins = (data, property) => {
+    const filteredResults = data.filter((filter) => filter.fields[property] !== undefined)
+    const getAllUniqueValues = filteredResults.reduce((acc, passenger) => {
+      const propertyValues = passenger.fields[property][0]
+      return { [propertyValues]: propertyValues, ...acc }
+    }, {}) 
   
-  //   return Object.values(getAllUniqueValues)
-  // }
-  // // console.log(getFirstLetterCabins(data, "cabin"))
+    return Object.values(getAllUniqueValues)
+  }
+  console.log(getFirstLetterCabins(data, "cabin"))
 
-  // const getCabinSurvival = (data) => {
-  //   const newArray = [];
-  //   const cabinLevels = getFirstLetterCabins(data, 'cabin')
-  //   const cabinSurvived = data.filter((cabin) => cabin.fields.cabin && cabin.fields.survived)
-  //   console.log(cabinSurvived)
-  //   const buckets = cabinLevels.length
-  //   // console.log(buckets)
 
-  //   for (let i = 0; i < buckets; i++){
-  //     newArray.push({
-  //       cabinLevel: "",
-  //       lived: 0,
-  //       died: 0,
-  //       total: 0,
-  //     })
-  //   }
+  const getCabinSurvival = (data, cabin, status, color) => {
+    const survived = status === 'Lived' ? 'Yes' : 'No';
+    return {
+      count: data.filter((passenger) => passenger.fields.cabin[0] === cabin && passenger.fields.survived === survived).length,
+      cabin: cabin,
+      status: status,
+      color: color
+    }
+  };
 
-  //   for (const passenger of cabinSurvived){
-  //     const bucket = passenger.fields.cabin[0]
-  //     console.log(`What is the bucket after going through the passenger : ${bucket}`)
-  //     console.log(newArray)
-  //     const index = newArray.findIndex((obj) => obj.cabinLevel === bucket)
-  //     console.log(newArray)
-  //     console.log(`This is the index: ${index}`)
-  //     newArray[index].total += 1;
-  //     passenger.fields.survived === "Yes" ? newArray[index].lived += 1 : newArray[index].died += 1;
-  //   }
-  //   // console.log(cabinLevels, cabinSurvived, buckets)
-  //   return newArray;
-  // }
+  const cabinWithSurvivors = [
+    getCabinSurvival(cabins, 'A', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'A', 'Died', "rgba(255, 0, 0, 0.5)"),
+    getCabinSurvival(cabins, 'B', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'B', 'Died', "rgba(255, 0, 0, 0.5)"),
+    getCabinSurvival(cabins, 'C', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'C', 'Died', "rgba(255, 0, 0, 0.5)"),
+    getCabinSurvival(cabins, 'D', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'D', 'Died', "rgba(255, 0, 0, 0.5)"),
+    getCabinSurvival(cabins, 'E', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'E', 'Died', "rgba(255, 0, 0, 0.5)"),
+    getCabinSurvival(cabins, 'F', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'F', 'Died', "rgba(255, 0, 0, 0.5)"),
+    getCabinSurvival(cabins, 'G', 'Lived', "rgba(60, 179, 113, 0.5)"),
+    getCabinSurvival(cabins, 'G', 'Died', "rgba(255, 0, 0, 0.5)"),
+  ]
 
-  // console.log(getCabinSurvival(data))
-
-  
+  console.log(cabinWithSurvivors) 
 
   const margin = { top: 30, right: 20, bottom: 10, left: 40 }
-  const width = 950 - (margin.left + margin.right)
+  const width = 800 - (margin.left + margin.right)
   const height = 500 - (margin.top + margin.bottom)
 
   const svg = d3.select('#svg_classSurvive')
-   
+
+  const xScale = d3.scaleBand()
+    .domain(cabinWithSurvivors)
+    .range([margin.left, width + margin.left])
+    .padding(0.05)
+
+  const xScaleName = d3.scaleBand()
+    .domain(cabinWithSurvivors.map(d => `${d.cabin} ${d.status}`))
+    .range([margin.left, width + margin.left])
+    .padding(0.05)
+
+  const cabinSurvivors = d3.extent(cabinWithSurvivors, d => d.count)
+  const yScale = d3.scaleLinear()
+    .domain(cabinSurvivors)
+    .range([height, margin.top])
+
+  const title = svg
+    .append('g')
+
+  title
+    .append('text')
+    .text('Status Of the Passengers with Cabin Information')
+    .attr('transform', `translate(${width / 2 - (margin.left + margin.right)}, 20)`)
+    .attr('class', 'graphText')
+
+  const barGroup = svg.append('g')
+
+  barGroup
+    .selectAll('rect')
+    .data(cabinWithSurvivors)
+    .enter()
+    .append('rect')
+    .attr('class', d => d.name)
+    .attr('x', d => xScale(d))
+    .attr('y', d => yScale(d.count))
+    .attr('height', d => height - yScale(d.count))
+    .attr('width', xScale.bandwidth())
+    .attr('stroke', "black")
+    .attr('fill', (d) => d.color)
+    .attr('opacity', 0.25)
+
+  const bottomAxis = d3.axisBottom(xScaleName)
+
+  const leftAxis = d3.axisLeft(yScale)
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(bottomAxis)
+
+  svg
+    .append('g')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(leftAxis)
+
+  
 }
 
 handleData()
--- script_embarkSurvive.js
+++ script_embarkSurvive.js
@@ -5,31 +5,101 @@ async function handleData() {
   const svg = d3.select('#svg_embarkSurvive')
 
   let data = await d3.json('titanic-passengers.json')
+  const embarked = data.filter((embarked) => embarked.fields.embarked && embarked.fields.survived)
+  console.log(embarked)
 
-  const embarkedIn = data.filter((passenger) => passenger.fields.embarked !== undefined)
-  // console.log(embarkedIn)
-  const survivedYes = data.filter((passenger) => passenger.fields.survived !== "Yes" & passenger.fields.embarked !== undefined)
-  const survivedNo = data.filter((passenger) => passenger.fields.survived !== "No" & passenger.fields.embarked !== undefined)
-  // console.log(survivedYes)
-  // console.log(survivedNo)
-
-  const getFilteredResults = (data, property) => {
+  const getFirstLetterembarkeds = (data, property) => {
     const filteredResults = data.filter((filter) => filter.fields[property] !== undefined)
     const getAllUniqueValues = filteredResults.reduce((acc, passenger) => {
-      const propertyValues = passenger.fields[property]
+      const propertyValues = passenger.fields[property][0]
       return { [propertyValues]: propertyValues, ...acc }
     }, {}) 
   
     return Object.values(getAllUniqueValues)
   }
-  // console.log(getFilteredResults(data, "embarked"))
+  console.log(getFirstLetterembarkeds(data, "embarked"))
+
+
+  const getembarkedSurvival = (data, embarked, status, color) => {
+    const survived = status === 'Lived' ? 'Yes' : 'No';
+    return {
+      count: data.filter((passenger) => passenger.fields.embarked === embarked && passenger.fields.survived === survived).length,
+      embarked: embarked,
+      status: status,
+      color: color
+    }
+  };
+
+  const embarkedWithSurvivors = [
+    getembarkedSurvival(embarked, 'S', 'Lived', "rgba(0, 0, 255, 1)"),
+    getembarkedSurvival(embarked, 'S', 'Died', "rgba(0, 0, 0, 1)"),
+    getembarkedSurvival(embarked, 'Q', 'Lived', "rgba(0, 0, 255, 1)"),
+    getembarkedSurvival(embarked, 'Q', 'Died', "rgba(0, 0, 0, 1)"),
+    getembarkedSurvival(embarked, 'C', 'Lived', "rgba(0, 0, 255, 1)"),
+    getembarkedSurvival(embarked, 'C', 'Died', "rgba(0, 0, 0, 1)"),
+  ]
+
+  console.log(embarkedWithSurvivors) 
+
+  const margin = { top: 30, right: 20, bottom: 10, left: 40 }
+  const width = 800 - (margin.left + margin.right)
+  const height = 500 - (margin.top + margin.bottom)
+
+  const xScale = d3.scaleBand()
+    .domain(embarkedWithSurvivors)
+    .range([margin.left, width + margin.left])
+    .padding(0.05)
+
+  const xScaleName = d3.scaleBand()
+    .domain(embarkedWithSurvivors.map(d => `${d.embarked} ${d.status}`))
+    .range([margin.left, width + margin.left])
+    .padding(0.05)
+
+  const embarkedSurvivors = d3.extent(embarkedWithSurvivors, d => d.count)
+  const yScale = d3.scaleLinear()
+    .domain(embarkedSurvivors)
+    .range([height, margin.top])
+
+  const title = svg
+    .append('g')
+
+  title
+    .append('text')
+    .text('Status Of the Passengers Who Embarked at Different Locations')
+    .attr('transform', `translate(${width / 2 - (margin.left + margin.right)}, 20)`)
+    .attr('class', 'graphText')
+
+  const barGroup = svg.append('g')
+
+  barGroup
+    .selectAll('rect')
+    .data(embarkedWithSurvivors)
+    .enter()
+    .append('rect')
+    .attr('class', d => d.name)
+    .attr('x', d => xScale(d))
+    .attr('y', d => yScale(d.count))
+    .attr('height', d => height - yScale(d.count))
+    .attr('width', xScale.bandwidth())
+    .attr('stroke', "black")
+    .attr('fill', (d) => d.color)
+    .attr('opacity', 0.25)
+
+  const bottomAxis = d3.axisBottom(xScaleName)
+
+  const leftAxis = d3.axisLeft(yScale)
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(bottomAxis)
+
+  svg
+    .append('g')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(leftAxis)
 
   
-
-
-
-
-
 }
 
 handleData()
